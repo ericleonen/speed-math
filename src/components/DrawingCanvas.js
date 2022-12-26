14092db4c7871b9afce8ade5ee770bd9c26d336a
ericleonen <ericleonen@gmail.com>
2022-12-25 21:59:17 -0800

    Add erase button

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const DrawingCanvas = () => {
     const [draw, setDraw] = useState(false);
@@ -69,13 +69,20 @@ const DrawingCanvas = () => {
         setDraw(true);
     };
 
+    const clearDraw = () => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext("2d");
+
+        context.reset();
+    }
+
     return (
-        <div>
+        <div className="flex flex-col items-center p-2 bg-gray-300">
             <canvas 
                 ref={canvasRef}
                 width="300"
                 height="300"
-                className="border-2 border-black"
+                className="border-4 border-gray-400 bg-white"
                 onMouseDown={startDraw}
                 onMouseUp={endDraw}
                 onMouseMove={onDraw}
@@ -85,6 +92,14 @@ const DrawingCanvas = () => {
                 onTouchEnd={endTouchDraw}
                 onTouchMove={onTouchDraw}
             />
+            <div className="mt-2 w-min rounded-sm overflow-hidden shadow-md border-[3px] border-gray-500">
+                <button 
+                    onClick={clearDraw}
+                    className="bg-gray-400/80 text-white border-2 border-white px-10 py-1 text-4xl active:bg-gray-400"
+                >
+                    Erase
+                </button>
+            </div>
         </div>
     );
 };
